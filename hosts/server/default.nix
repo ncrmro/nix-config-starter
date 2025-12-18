a45141a5ac70760d5d9caa696d5dc1960204cf43
Nicholas Romero <ncrmro@gmail.com>
2025-12-17 23:39:58 -0600

    feat: add testvm

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "jdoe-home-server";
+  networking.hostName = "username-home-server";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -20,8 +20,15 @@
 
   programs.zsh.enable = true;
 
+  # Enable SSH access
+  services.openssh.enable = true;
+
+  # Root user SSH access (replace with your key
+  users.users.root.openssh.authorizedKeys.keys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyr ...." ];
+
   # Define a user account.
-  users.users.jdoe = {
+  users.users.username = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
@@ -37,6 +44,6 @@
     gc.dates = "weekly";
     gc.options = "--delete-older-than 7d";
   };
-  
+
   system.stateVersion = "24.05";
 }
